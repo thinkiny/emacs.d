a9ae26329e367e744db7b8f4c85d4a6cbd81aa4c
thinkiny <thinkiny@126.com>
2022-06-13 20:05:35 +0800

    use pyright

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -4,24 +4,35 @@
               auto-mode-alist))
 
 (require-package 'pip-requirements)
+(require-package 'lsp-pyright)
 
 (defvar-local lsp-pylsp-extra-paths [])
 (defvar-local lsp-pylsp-cache-for [])
 (with-eval-after-load 'lsp-mode
-  ;;(setq lsp-pylsp-plugins-yapf-enabled t)
-  (lsp-register-custom-settings '(("pylsp.plugins.jedi.extra_paths" lsp-pylsp-extra-paths)
-                                  ("pylsp.plugins.jedi_completion.cache_for" lsp-pylsp-cache-for)))
+  (require 'lsp-pyright)
   (lsp-register-client
-   (make-lsp-client :new-connection (lsp-tramp-connection-fast
-                                     (lambda () lsp-pylsp-server-command))
-                    :major-modes '(python-mode cython-mode)
-                    :priority -1
-                    :remote? t
-                    :server-id 'pylsp-remote
-                    :library-folders-fn (lambda (_workspace) lsp-clients-pylsp-library-directories)
-                    :initialized-fn (lambda (workspace)
-                                      (with-lsp-workspace workspace
-                                        (lsp--set-configuration (lsp-configuration-section "pylsp")))))))
+   (make-lsp-client
+    :new-connection (lsp-tramp-connection-fast (lambda ()
+                                                 (cons "pyright-python-langserver"
+                                                       lsp-pyright-langserver-command-args)))
+    :major-modes '(python-mode cython-mode)
+    :multi-root lsp-pyright-multi-root
+    :priority -1
+    :remote? t
+    :server-id 'pyright-remote
+    :priority -1
+    :initialized-fn (lambda (workspace)
+                      (with-lsp-workspace workspace
+                        ;; we send empty settings initially, LSP server will ask for the
+                        ;; configuration of each workspace folder later separately
+                        (lsp--set-configuration
+                         (make-hash-table :test 'equal))))
+    :download-server-fn (lambda (_client callback error-callback _update?)
+                          (lsp-package-ensure 'pyright callback error-callback))
+    :notification-handlers (lsp-ht ("pyright/beginProgress" 'lsp-pyright--begin-progress-callback)
+                                   ("pyright/reportProgress" 'lsp-pyright--report-progress-callback)
+                                   ("pyright/endProgress" 'lsp-pyright--end-progress-callback))))
+)
 
 (add-hook 'python-mode-hook (lambda ()
                               (setq-local lsp-enable-format-at-save nil)
