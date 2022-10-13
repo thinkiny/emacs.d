b52dcdc88e8e695f5abf767f86ae8426ee95b4d6
thinkiny <thinkiny@126.com>
2022-10-13 19:07:38 +0800

    disable cape-dabbrev

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -61,6 +61,7 @@
 (use-package cape
   :init
   (add-to-list 'completion-at-point-functions #'cape-file)
-  (add-to-list 'completion-at-point-functions #'cape-dabbrev))
+  ;; (add-to-list 'completion-at-point-functions #'cape-dabbrev)
+  )
 
 (provide 'init-company)
