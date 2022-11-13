a2048332c174dc25a8107745df6f83b8aad6d15d
thinkiny <thinkiny@126.com>
2022-11-13 23:40:52 +0800

    use eglot in python

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -4,35 +4,6 @@
               auto-mode-alist))
 
 (require-package 'pip-requirements)
-(require-package 'lsp-pyright)
-
-(defvar-local lsp-pylsp-extra-paths [])
-(defvar-local lsp-pylsp-cache-for [])
-(with-eval-after-load 'lsp-mode
-  (require 'lsp-pyright)
-  (lsp-register-client
-   (make-lsp-client
-    :new-connection (lsp-tramp-connection-fast (lambda ()
-                                                 (cons "pyright-python-langserver"
-                                                       lsp-pyright-langserver-command-args)))
-    :major-modes '(python-mode cython-mode)
-    :multi-root lsp-pyright-multi-root
-    :priority -1
-    :remote? t
-    :server-id 'pyright-remote
-    :priority -1
-    :initialized-fn (lambda (workspace)
-                      (with-lsp-workspace workspace
-                        ;; we send empty settings initially, LSP server will ask for the
-                        ;; configuration of each workspace folder later separately
-                        (lsp--set-configuration
-                         (make-hash-table :test 'equal))))
-    :download-server-fn (lambda (_client callback error-callback _update?)
-                          (lsp-package-ensure 'pyright callback error-callback))
-    :notification-handlers (lsp-ht ("pyright/beginProgress" 'lsp-pyright--begin-progress-callback)
-                                   ("pyright/reportProgress" 'lsp-pyright--report-progress-callback)
-                                   ("pyright/endProgress" 'lsp-pyright--end-progress-callback))))
-)
 
 (defun my-python-mode-hook()
   (setq-local lsp-enable-format-at-save nil)
