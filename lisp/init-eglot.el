29ec05ca0217c941a6bfc275318df2246ba7a5a8
thinkiny <thinkiny@126.com>
2022-11-14 00:53:54 +0800

    add some keybindings

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -1,5 +1,13 @@
-(use-package eglot)
+(use-package eglot
+  :hook (eglot-managed-mode . my-eglot-mode-hook))
 
 (use-package consult-eglot)
+
+(defun my-eglot-mode-hook()
+  (define-key eglot-mode-map (kbd "C-c r") 'eglot-rename)
+  (define-key eglot-mode-map (kbd "C-c i") 'eglot-code-action-organize-imports)
+  (define-key eglot-mode-map (kbd "C-c e") 'flymake-show-buffer-diagnostics)
+  (define-key eglot-mode-map (kbd "C-c h") 'eldoc-doc-buffer)
+  (define-key eglot-mode-map (kbd "C-c f") 'eglot-code-action-quickfix))
 
 (provide 'init-eglot)
