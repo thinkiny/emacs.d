3ce644c0ea329d5597d5683e48a1f7b1288cde96
thinkiny <thinkiny@126.com>
2022-11-17 14:15:04 +0800

    enable format

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -3,6 +3,20 @@
 
 (use-package consult-eglot)
 
+;; format
+(defvar-local eglot-enable-format-at-save t)
+(defun eglot-enable-format ()
+  (interactive)
+  (whitespace-cleanup-mode 1)
+  (add-hook 'before-save-hook 'eglot-format-buffer nil 'eglot-format)
+  (setq-local eglot-enable-format-at-save t))
+
+(defun eglot-disable-format ()
+  (interactive)
+  (whitespace-cleanup-mode 0)
+  (remove-hook 'before-save-hook 'eglot-format-buffer 'eglot-format)
+  (setq-local eglot-enable-format-at-save nil))
+
 (defun my-eglot-mode-hook()
   (define-key eglot-mode-map (kbd "C-c r") 'eglot-rename)
   (define-key eglot-mode-map (kbd "C-c i") 'eglot-code-action-organize-imports)
@@ -11,6 +25,8 @@
   (define-key eglot-mode-map (kbd "C-c v") 'eglot-find-implementation)
   (define-key eglot-mode-map (kbd "C-c f") 'eglot-code-action-quickfix)
   (eglot--setq-saving eldoc-documentation-functions
-                        '(eglot-signature-eldoc-function)))
+                      '(eglot-signature-eldoc-function))
+  (if eglot-enable-format-at-save
+      (eglot-enable-format)))
 
 (provide 'init-eglot)
