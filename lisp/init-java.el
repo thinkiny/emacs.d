c691bee43743c9db0d156809e4e21eb310dbd42c
thinkiny <thinkiny@126.com>
2022-12-03 00:26:31 +0800

    update init-java

--- lisp/init-java.el
+++ lisp/init-java.el
@@ -17,12 +17,9 @@
   (google-set-c-style-with-offset 4)
   (c-toggle-auto-newline -1)
   (setq fill-column 100)
-  (local-set-key (kbd "C-c j n") #'eglot-java-file-new)
-  (local-set-key (kbd "C-c j r") #'eglot-java-run-main)
-  (local-set-key (kbd "C-c j t") #'eglot-java-run-test)
-  (local-set-key (kbd "C-c j N") #'eglot-java-project-new)
-  (local-set-key (kbd "C-c j T") #'eglot-java-project-build-task)
-  (local-set-key (kbd "C-c j R") #'eglot-java-project-build-refresh))
+  (local-set-key (kbd "C-c t") #'eglot-java-run-test)
+  (local-set-key (kbd "C-c b") #'eglot-java-project-build-task)
+  (local-set-key (kbd "C-c u") #'eglot-java-project-build-refresh))
 
 (add-hook 'java-mode-hook #'my-java-hook)
 
