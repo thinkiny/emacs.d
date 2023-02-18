c3b5c3c9e6bb494cf950da2dbfc7bbcc0d73b365
thinkiny <thinkiny@126.com>
2023-02-18 11:11:48 +0800

    update epub

--- lisp/init-epub.el
+++ lisp/init-epub.el
@@ -26,11 +26,14 @@
   (if (boundp 'mwheel-scroll-down-function)
       (setq-local mwheel-scroll-down-function
                   #'nov-goto-previous-line-or-page))
+  (setq-local line-spacing 1.0)
   (define-key nov-mode-map (kbd "N") 'nov-next-document)
   (define-key nov-mode-map (kbd "P") 'nov-previous-document)
   (define-key nov-mode-map (kbd "n") 'nov-goto-next-line-or-page)
   (define-key nov-mode-map (kbd "p") 'nov-goto-previous-line-or-page)
   (define-key nov-mode-map (kbd "o") 'nov-goto-toc)
+  (define-key nov-mode-map (kbd "f") 'forward-char)
+  (define-key nov-mode-map (kbd "b") 'backward-char)
   (define-key nov-mode-map (kbd "=") 'er/expand-region)
   (define-key nov-mode-map (kbd ",") 'bing-dict-at-point))
 
