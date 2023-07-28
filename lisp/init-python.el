f86c96e7e20489a79903965a468fdf5e5e9f3986
thinkiny <thinkiny@126.com>
2023-07-28 20:26:27 +0800

    update python

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -5,11 +5,15 @@
 
 (require-package 'pip-requirements)
 
+(use-package pyvenv-auto
+  :custom
+  (pyvenv-auto-mode t))
+
 (defun my-python-mode-hook()
   (setq-local lsp-enable-format-at-save nil)
   (eglot-ensure))
 
 (add-hook 'python-mode-hook #'my-python-mode-hook)
-
+(add-hook 'python-ts-mode-hook #'my-python-mode-hook)
 
 (provide 'init-python)
