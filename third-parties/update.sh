57811be49542dba88b3833249223b8c9abbbe9f2
thinkiny <thinkiny@126.com>
2023-08-09 20:51:17 +0800

    update pdf-tools

--- third-parties/update.sh
+++ third-parties/update.sh
@@ -21,11 +21,13 @@ wget https://raw.githubusercontent.com/dwijnand/sbt-extras/master/sbt -O sbt
 wget https://raw.githubusercontent.com/chenyanming/nov-xwidget/main/nov-xwidget.el -O nov-xwidget.el
 
 if [ ! -d "pdf-tools" ]; then
-    git clone --branch pdf-roll https://github.com/dalanicolai/pdf-tools.git
+    #git clone --branch pdf-roll https://github.com/dalanicolai/pdf-tools.git
+    git clone --branch continuous-scroll https://github.com/aikrahguzar/pdf-tools.git
 fi
 
 build_pdf_tools pdf-tools
 
-wget https://raw.githubusercontent.com/dalanicolai/image-roll.el/main/image-roll.el -O pdf-tools/lisp/image-roll.el
+#wget https://raw.githubusercontent.com/dalanicolai/image-roll.el/main/image-roll.el -O pdf-tools/lisp/image-roll.el
+wget https://raw.githubusercontent.com/aikrahguzar/image-roll.el/main/image-roll.el -O pdf-tools/lisp/image-roll.el
 wget https://raw.githubusercontent.com/007kevin/pdf-view-restore/master/pdf-view-restore.el -O pdf-tools/lisp/pdf-view-restore.el
 wget https://raw.githubusercontent.com/KaranAhlawat/scala-ts-mode/main/scala-ts-mode.el -O scala-ts-mode.el
