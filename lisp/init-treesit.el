a62e712c5ec8aa8365fe52f702f9e52a7c2de70f
thinkiny <thinkiny@126.com>
2023-09-14 13:47:58 +0800

    set treesit-language-source-alist

--- lisp/init-treesit.el
+++ lisp/init-treesit.el
@@ -10,6 +10,24 @@
   (after-load-theme (set-face-attribute 'tree-sitter-hl-face:property nil :slant 'normal)))
 
 
+(setq treesit-language-source-alist
+   '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+     (cmake "https://github.com/uyha/tree-sitter-cmake")
+     (css "https://github.com/tree-sitter/tree-sitter-css")
+     (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+     (go "https://github.com/tree-sitter/tree-sitter-go")
+     (html "https://github.com/tree-sitter/tree-sitter-html")
+     (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+     (json "https://github.com/tree-sitter/tree-sitter-json")
+     (make "https://github.com/alemuller/tree-sitter-make")
+     (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+     (python "https://github.com/tree-sitter/tree-sitter-python")
+     (toml "https://github.com/tree-sitter/tree-sitter-toml")
+     (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+     (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+     (yaml "https://github.com/ikatyang/tree-sitter-yaml")
+     (scala "https://github.com/tree-sitter/tree-sitter-scala")))
+
 (defun treesit-init()
   (setq treesit-use-build-in t)
   (require 'treesit)
