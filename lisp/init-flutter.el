ad0a916a5392588173684793624b64060ee2fa4e
thinkiny <thinkiny@126.com>
2024-04-08 02:07:21 +0800

    feat: update init-flutter

--- lisp/init-flutter.el
+++ lisp/init-flutter.el
@@ -8,10 +8,16 @@
   (if (flutter--running-p)
       (flutter-hot-reload)))
 
+(defun flutter-maybe-restart()
+  (interactive)
+  (if (flutter--running-p)
+      (flutter-hot-restart)
+    (flutter-run)))
+
 (defun my-dart-mode-hook()
   (eglot-ensure)
   (flutter-test-mode)
-  (define-key dart-mode-map (kbd "C-c <RET>") #'flutter-run)
+  (define-key dart-mode-map (kbd "C-c <RET>") #'flutter-maybe-restart)
   (add-hook 'after-save-hook 'flutter-maybe-hotload nil t))
 
 (provide 'init-flutter)
