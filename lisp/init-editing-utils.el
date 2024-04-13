0485b0b568e939fbd3c2bb3aef2a32a227d8c8d5
thinkiny <thinkiny@126.com>
2024-04-13 15:29:47 +0800

    feat: set process-adaptive-read-buffering

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -38,7 +38,8 @@
 (global-set-key (kbd "C-c $") 'toggle-truncate-lines)
 
 ;;; Optimizations
-(setq read-process-output-max (* 2048 1024))
+(setq process-adaptive-read-buffering nil)
+(setq read-process-output-max (* 4 1024 1024))
 ;; A second, case-insensitive pass over `auto-mode-alist' is time wasted, and
 ;; indicates misconfiguration (don't rely on case insensitivity for file names).
 (setq auto-mode-case-fold nil)
