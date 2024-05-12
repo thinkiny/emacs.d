4c1b5c355050be8ed19f11d73ad523d84cd433ba
thinkiny <thinkiny@126.com>
2024-05-13 00:01:28 +0800

    feat: update snippets and performance

--- lisp/init-performance.el
+++ lisp/init-performance.el
@@ -1,9 +1,26 @@
-(use-package gcmh
-  :demand t
-  :config
-  (setq gcmh-low-cons-threshold (* 32 1024 1024)
-        gcmh-high-cons-threshold (* 512 1024 1024))
-  (gcmh-mode 1))
+;; (use-package gcmh
+;;   :demand t
+;;   :config
+;;   (setq gcmh-low-cons-threshold (* 32 1024 1024)
+;;         gcmh-high-cons-threshold (* 512 1024 1024))
+;;   (gcmh-mode 1))
+(setq gc-cons-threshold (* 4 1024 1024 1024)) ;; 4GiB of RAM
+
+(defvar my/gc-timer nil
+  "Timer for garbage collection. See
+`my/garbage-collect-on-focus-lost'.")
+
+(defun my/garbage-collect-on-focus-lost ()
+  "Garbage collect when Emacs loses focus.
+
+Garbage collection is only triggered thirty seconds after losing
+focus, and only once."
+  (if (frame-focus-state)
+      (when (timerp my/gc-timer)
+       (cancel-timer my/gc-timer))
+    (setq my/gc-timer (run-with-idle-timer 5 nil #'garbage-collect))))
+
+(add-function :after after-focus-change-function #'my/garbage-collect-on-focus-lost)
 
 (defun profile-cpu-start ()
   (interactive)
