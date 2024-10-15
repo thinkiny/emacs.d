2ad5b9e95c3b666dc832d2ef33d2bd8d03c69752
thinkiny <thinkiny@126.com>
2024-10-15 10:56:03 +0800

    feat: tweak elfeed settings

--- lisp/init-feed.el
+++ lisp/init-feed.el
@@ -18,10 +18,13 @@ browser defined by `browse-url-generic-program'."
       (with-current-buffer (xwidget-webkit-get-browse-buffer)
         (add-hook 'quit-window-hook #'kill-elfeed-buffer nil t))))
 
-  (defun my-elfeed-show-mode-hook()
-    (visual-line-mode))
 
-  (with-eval-after-load 'elfeed-show
+  (with-eval-after-load 'elfeed-search
+    (define-key elfeed-search-mode-map (kbd "j") #'next-line)
+    (define-key elfeed-search-mode-map (kbd "k") #'previous-line))
+
+  (defun my-elfeed-show-mode-hook()
+    (visual-line-mode)
     (define-key elfeed-show-mode-map (kbd "n") #'scroll-down-line)
     (define-key elfeed-show-mode-map (kbd "b") #'backward-char)
     (define-key elfeed-show-mode-map (kbd "f") #'forward-char)
@@ -29,7 +32,7 @@ browser defined by `browse-url-generic-program'."
     (define-key elfeed-show-mode-map (kbd "e") #'end-of-line)
     (define-key elfeed-show-mode-map (kbd ",") #'bing-dict-at-point)
     (define-key elfeed-show-mode-map (kbd "v") #'scroll-up)
-    (define-key elfeed-show-mode-map (kbd "M-v") #'scroll-down)
+    ;;(define-key elfeed-show-mode-map (kbd "M-v") #'scroll-down)
     (define-key elfeed-show-mode-map (kbd "<RET>") #'elfeed-show-visit)
     (define-key elfeed-show-mode-map (kbd "p") #'scroll-up-line)
     (define-key elfeed-show-mode-map (kbd "j") #'scroll-down-line)
@@ -37,12 +40,10 @@ browser defined by `browse-url-generic-program'."
     (define-key elfeed-show-mode-map (kbd "w") #'elfeed-show-yank)
     (define-key elfeed-show-mode-map (kbd "N") #'elfeed-show-next)
     (define-key elfeed-show-mode-map (kbd "P") #'elfeed-show-prev)
-    (define-key elfeed-show-mode-map (kbd "M-v") #'scroll-down-command)
     (define-key elfeed-show-mode-map (kbd "<double-mouse-1>") #'bing-dict-at-point))
 
-  (add-hook 'elfeed-show-mode-hook #'my-elfeed-show-mode-hook)
-  (define-key elfeed-search-mode-map (kbd "j") #'next-line)
-  (define-key elfeed-search-mode-map (kbd "k") #'previous-line))
+  (elfeed-goodies/setup)
+  (add-hook 'elfeed-show-mode-hook #'my-elfeed-show-mode-hook))
 
 (defun kill-elfeed-buffer()
   (let* ((buffer (get-buffer "*elfeed-entry*")))
@@ -50,9 +51,14 @@ browser defined by `browse-url-generic-program'."
 
 (use-package elfeed-goodies
   :after elfeed
-  :ensure t)
-
-(elfeed-goodies/setup)
+  :ensure t
+  :config
+  (defun elfeed-goodies/show-mode-setup ()
+  "Setup function providing defaults for show mode buffer."
+  (setq header-line-format '(:eval (elfeed-goodies/entry-header-line))
+        left-margin-width elfeed-goodies/show-mode-padding
+        right-margin-width elfeed-goodies/show-mode-padding)
+  (define-key elfeed-show-mode-map (kbd "M-v") #'scroll-down)))
 
 ;; setup feeds
 (defun feed-github-commit (repo)
