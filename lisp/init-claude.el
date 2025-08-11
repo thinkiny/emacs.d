42ac85ccefed895b713eafae445633def79ba055
thinkiny <thinkiny@126.com>
2025-08-11 14:20:03 +0800

    feat: update ui

--- lisp/init-claude.el
+++ lisp/init-claude.el
@@ -8,7 +8,7 @@
   (claude-code-ide-emacs-tools-setup)
   (setq claude-code-ide-diagnostics-backend 'flymake)
   (setq claude-code-ide-window-side 'bottom
-        claude-code-ide-window-height 30)
+        claude-code-ide-window-height 10)
   ;; (setenv "ANTHROPIC_BASE_URL" "https://api.moonshot.cn/anthropic/")
   ;; (setenv "ANTHROPIC_MODEL" "kimi-k2-turbo-preview")
   ;; (setenv "ANTHROPIC_SMALL_FAST_MODEL" "kimi-k2-turbo-preview")
