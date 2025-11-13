3c9938c6e3a1a304ff0049f5d2a5b8b55e499684
thinkiny <thinkiny@126.com>
2025-11-13 19:06:09 +0800

    feat: update ui

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -15,7 +15,7 @@
   (setq mouse-avoidance-mode 'animate))
 
 (with-eval-after-load 'fringe
-  (set-fringe-mode '(0 . 0)))
+  (set-fringe-mode '(1 . 0)))
 
 (setq frame-resize-pixelwise t)
 (setq indicate-empty-lines t)
